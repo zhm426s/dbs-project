22214c8324b03b6a19509c66288880ffd83b92ab
flash2005mc-sketch <flash2005mc@gmail.com>
2026-03-30 17:50:50 -0400

    tentative setup i was envisioning, will try to figure out reading/updating conditions later

--- src/HMSTesting.java
+++ src/HMSTesting.java
@@ -1,7 +1,27 @@
+
 public class HMSTesting {
 
     public static void main(String[] args) {
-        Patient newPatient = new Patient("123-98-7293", "Jane Doe", "1984-02-02", 'F', "johndoe2@example.org", "123-555-8298", "KDSO4338UJFS", "Aetna", 0.13, "Bronze Plan", "Migraines");
-        newPatient.addPatient();
+        PatientImpl patientDAO = new PatientImpl();
+
+        Patient newPatient = new Patient("123-09-1929", "Jimmy Doe", "1948-02-02", 'M', "jimdoe@example.org", "123-555-8911", "JJO1298D723D", "Aetna", 0.13, "Bronze Plan", "Migraines");
+        // patientDAO.addPatient(newPatient);
+        // printPatientInfo(patientDAO.getPatient("123-09-1929"));
+        /*ArrayList<Patient> patients = patientDAO.getAllPatients();
+        for (Patient p : patients) {
+            printPatientInfo(p);
+            System.out.println("-----");
+        }*/
+    }
+
+    public static void printPatientInfo(Patient patient) {
+        System.out.println("SSN: " + patient.ssn);
+        System.out.println("Name: " + patient.name);
+        System.out.println("DOB: " + patient.dob);
+        System.out.println("BioSex: " + patient.bioSex);
+        System.out.println("Email: " + patient.email);
+        System.out.println("Phone: " + patient.phone);
+        System.out.println("Insurance ID: " + patient.insID);
+        System.out.println("Conditions: " + patient.conditions);
     }
 }
